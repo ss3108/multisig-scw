1b3d31a8536c776736489917cdc282578d7b3834
ss3108 <shrutisingh3108@gmail.com>
2024-01-05 13:08:43 +0530

    s => g

--- contracts/script/WalletFactory.s.sol
+++ contracts/script/WalletFactory.s.sol
@@ -6,15 +6,16 @@ import "../src/WalletFactory.sol";
 import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
 
 contract WalletFactoryScript is Script {
+    // Address of the EntryPoint contract on Goerli
     IEntryPoint constant ENTRYPOINT =
         IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789);
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); // Fetch the private key from environment variables
+        vm.startBroadcast(deployerPrivateKey); // Start broadcasting transactions
 
-        WalletFactory walletFactory = new WalletFactory(ENTRYPOINT);
+        WalletFactory walletFactory = new WalletFactory(ENTRYPOINT); // Initialize the WalletFactory contract
 
-        vm.stopBroadcast();
+        vm.stopBroadcast(); // Stop broadcasting transactions
     }
 }
