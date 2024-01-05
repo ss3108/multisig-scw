454df16e58c13fc47d3705da7abbadafdc6fcc6e
ss3108 <shrutisingh3108@gmail.com>
2024-01-05 10:57:51 +0530

    src

---
+++ src/components/footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+// Define the Footer function
+export default function Footer() {
+  return (
+    <div className="w-full px-6 border-t border-t-gray-700 py-2 text-center">
+      Build your own account-abstracted multisig wallet with{" "}
+      <Link
+        href="https://learnweb3.io/"
+        target="_blank"
+        className="text-blue-500 hover:text-blue-600"
+      >
+        LearnWeb3
+      </Link>
+      !
+    </div>
+  );
+}
